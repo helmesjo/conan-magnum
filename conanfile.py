544ba44908336bfc6d7f72b89e57fce225e25954
helmesjo <helmesjo@live.com>
2019-01-20 15:11:38 +0100

    Default 'build_deprecated' to 'True' (to be consistent with corrade)

--- conanfile.py
+++ conanfile.py
@@ -84,7 +84,7 @@ class LibnameConan(ConanFile):
     default_options = {
         "shared": False, 
         "fPIC": True,
-        "build_deprecated": False,
+        "build_deprecated": True,
         "build_multithreaded": True,
         "build_plugins_static": False,
         "build_tests": False,
